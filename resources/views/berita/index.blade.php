dfe2c03f7944941631fd17a04ff551ca23c18fdf
Asadel123 <adel090103@gmail.com>
2025-05-12 22:39:53 +0700

    Menambahkan halaman home untuk menampilkan daftar berita

--- resources/views/berita/index.blade.php
+++ resources/views/berita/index.blade.php
@@ -5,6 +5,6 @@
 </head>
 <body>
     <h1>Selamat Datang di Portal Berita</h1>
-    <p>Ini adalah halaman utama portal berita Laravel kamu.</p>
+    <p>Ini adalah halaman utama portal berita yang terpercaya.</p>
 </body>
 </html>
