9b0c4006646c51195bea657694e39934376c3ccd
Asadel123 <adel090103@gmail.com>
2025-05-12 22:44:00 +0700

    Menambahkan routing untuk halaman berita dan detail berita

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\BeritaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [BeritaController::class, 'index']);
+// Halaman utama untuk menampilkan daftar berita
+Route::get('/', [BeritaController::class, 'index'])->name('home');
+
+// Halaman detail berita berdasarkan ID
+Route::get('/berita/{id}', [BeritaController::class, 'show'])->name('berita.show');
